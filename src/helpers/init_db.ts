601c1e3b68e2e7762fda92c97fdc2a67ddb487d6
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-05-27 22:19:40 +0545

    database connection

---
+++ src/helpers/init_db.ts
@@ -0,0 +1,26 @@
+import { connect, connection } from "mongoose"
+const { MONGO_URI, DB_NAME } = process.env as { [key: string]: string }
+
+connect(MONGO_URI, { dbName: DB_NAME })
+    .then(() => {
+        console.log("mongodb connected.")
+    })
+    .catch((err) => console.log(err.message))
+
+connection.on("connected", () => {
+    console.log("Mongoose connected to db")
+})
+
+connection.on("error", (err) => {
+    console.log(err.message)
+})
+
+connection.on("disconnected", () => {
+    console.log("Mongoose connection is disconnected.")
+})
+
+// graceful shutdown
+process.on("SIGINT", async () => {
+    await connection.close()
+    process.exit(0)
+})
