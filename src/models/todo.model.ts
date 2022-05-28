2677b198cf3c8ed561a4566553d25c71293312b0
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-05-28 08:47:52 +0545

    add schema

---
+++ src/models/todo.model.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose"
+import { ITodo } from "../types/todo.types"
+
+const todoTaskSchema = new Schema<ITodo>({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: Boolean,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+})
+
+// convert _id => id on response
+todoTaskSchema.set("toJSON", {
+    virtuals: true,
+})
+
+const Todo = model<ITodo>("TodoTask", todoTaskSchema)
+export default Todo
