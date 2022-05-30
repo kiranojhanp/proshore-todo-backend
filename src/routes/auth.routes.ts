a4dbd481580067198d09014d0a74a73108a72026
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-05-30 07:45:41 +0545

    use regex to match password

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,6 +4,6 @@ import { login, register } from "../controllers/auth.controller"
 const router = express.Router()
 
 router.post("/login", login)
-router.post("/register", register)
+router.post("/signup", register)
 
 export default router
--- src/validations/auth.schema.ts
+++ src/validations/auth.schema.ts
@@ -7,6 +7,11 @@ export const loginSchema = Joi.object({
 
 export const signupSchema = Joi.object({
     email: Joi.string().email().lowercase().required(),
-    password: Joi.string().required(),
+    password: Joi.string()
+        .required()
+        .regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+            "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+        ),
     confirmPassword: Joi.string().required().valid(Joi.ref("password")),
 })
