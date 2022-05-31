f1387c5c65f14eeb2839b947a05e8cefb2b7f3bd
Kiran Ojha <54209728+kiranojhanp@users.noreply.github.com>
2022-06-01 00:41:24 +0545

    use redis

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 import createHttpError from "http-errors"
+import { client } from "../helpers/init_redis"
 import Todo, { TodoType } from "../models/todo.model"
 import { todoSchema } from "../validations/todo.schema"
 
@@ -7,15 +8,48 @@ import { todoSchema } from "../validations/todo.schema"
 const getTodos = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const status = req.query.status || ""
-        const user = req.user.aud
+        const user = req.user.aud as string
+
         let todos
 
         if (status === "done") {
+            // get done todos from redis memory if already available
+            const donetodos = await client.GET("done-todos" + user)
+            if (donetodos) {
+                res.send(JSON.parse(donetodos))
+                return
+            }
+
+            // get done todos from mongodb if not available in redis
             todos = await Todo.find({ user, status: true })
+
+            // cache data every 1 second
+            // database operations are expensive so caching is a necessity
+            // set 1 second as todo list is expected to be updated by users very frequently
+            await client.setEx("done-todos" + user, 1, JSON.stringify(todos))
         } else if (status === "upcoming") {
+            // get upcoming todos from redis memory if already available
+            const upcomingtodos = await client.GET("upcoming-todos" + user)
+            if (upcomingtodos) {
+                res.send(JSON.parse(upcomingtodos))
+                return
+            }
+
+            // get upcoming todos from mongodb if not available in redis
             todos = await Todo.find({ user, status: false })
+            await client.setEx("upcoming-todos" + user, 1, JSON.stringify(todos))
         } else {
+            // get todos from redis memory if already available
+            const alltodos = await client.GET("todos" + user)
+
+            if (alltodos) {
+                res.send(JSON.parse(alltodos))
+                return
+            }
+
+            // get todos from mongodb if not available in redis
             todos = await Todo.find({ user })
+            await client.setEx("todos" + user, 2, JSON.stringify(todos))
         }
 
         if (!todos) throw new createHttpError.NotFound()
@@ -43,6 +77,7 @@ const addTodo = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const user = req.user.aud
         const { name, description, status, date } = req.body
+        // validate form data using joi schema
         const result = await todoSchema.validateAsync({ user, name, description, status, date })
         const todo = new Todo(result)
         const saveTodo = await todo.save()
@@ -57,8 +92,20 @@ const getTodo = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const { aud } = req.user
         const { id } = req.params
+
+        // get todo item from redis memory if already available
+        const todoItem = await client.GET("todo-item" + id)
+        if (todoItem) {
+            res.send(JSON.parse(todoItem))
+            return
+        }
+
+        // either throws notfound error, or resolves the promise with todo item data
         const todo = await getIfDataExists(id)
+        // either throws unauthorized error, or resolves the promise with true
         await checkOwner(todo, aud)
+        // cache the todo-item in redis
+        await client.setEx("todo-item" + id, 1, JSON.stringify(todo))
         res.status(200).json(todo)
     } catch (error) {
         next(error)
